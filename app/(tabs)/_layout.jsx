50bb3df3d3da03214a2b9554b8e746c890f91a42
Suhasini Padmanathan <suhasini_95@hotmail.com>
2024-06-05 17:29:03 -0400

    assets loaded with expo-asset package and front page layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -36,6 +36,14 @@ const TabsLayout = () => {
       <Tabs
         screenOptions={{
           tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
         }}
       >
         <Tabs.Screen
--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,38 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Expense Tracker</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4 ">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px] "
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-2xl text-white font-bold text-center">
+              Track your expenses and groceries with
+              <Text className=" text-secondary-200"> Cheap-ify </Text>
+            </Text>
+            <Image
+              source={images.path}
+              className=" w-[160px] h-[20px] absolute -bottom-4 -right-5"
+              resizeMode="contain"
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 export default App;
