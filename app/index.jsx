de566849234ee5012e0b2822bea48d7c5dae60c1
Suhasini Padmanathan <suhasini_95@hotmail.com>
2024-06-05 18:07:54 -0400

    front page of app done

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,15 @@
 import { ScrollView, Text, View, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 const App = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4 ">
+        <View className="w-full flex justify-center items-center h-full px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -26,12 +27,24 @@ const App = () => {
             </Text>
             <Image
               source={images.path}
-              className=" w-[160px] h-[20px] absolute -bottom-4 -right-5"
+              className=" w-[160px] h-[20px] absolute -bottom-4 -right-4 "
               resizeMode="contain"
             />
           </View>
+
+          <Text className="text-gray-100 text-sm font-pregular mt-7">
+            Transform Your Spending Habits with an Easy-to-Use Expense Tracker.
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {
+              router.push("/sign-in");
+            }}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 };
