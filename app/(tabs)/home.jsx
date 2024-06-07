c396088038b1f4a6f70709423ad47682c3dc77e3
Suhasini Padmanathan <suhasini_95@hotmail.com>
2024-06-07 16:19:54 -0400

    video list feature

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,27 +1,40 @@
-import { View, Text, FlatList, Image, RefreshControl } from "react-native";
-import { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
+import { getAllPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+
 // scroll view doesnt support horizontal and vertical flat lists at the same time
 // therefore use a flatlist (horizontal) within the header component of a flatlist (vertical)
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  // console.log(posts);
+
   const onRefresh = async () => {
     setRefreshing(true);
-    //recall videos to see if any videos appear
+    await refetch();
     setRefreshing(false);
   };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ $id: 1 }, { $id: 2 }, { $id: 3 }]}
+        data={posts}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.$id}</Text>
-        )}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
---
+++ components/VideoCard.jsx
@@ -0,0 +1,83 @@
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons/faEllipsisVertical";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons/faCirclePlay";
+
+const VideoCard = ({
+  video: {
+    title,
+    thumbnail,
+    video,
+    creator: { username, avatar },
+  },
+}) => {
+  const [play, setPlay] = useState(false);
+
+  return (
+    <View className="flex-col items-center px-4 mb-14">
+      <View className="flex-row gap-3 items-start">
+        <View className="justify-center items-center flex-row flex-1">
+          <View
+            className="w-[46px] h-[46px] rounded-lg border border-secondary 
+          justify-center items-center p-0.5"
+          >
+            <Image
+              source={{ uri: avatar }}
+              className="w-full h-full rounded-lg"
+              resizeMode="cover"
+            />
+          </View>
+          <View className="justify-center flex-1 ml-3 gap-y-1">
+            <Text
+              className="text-sm text-white font-psemibold"
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
+            <Text
+              className="text-xs text-gray-100 font-pregular"
+              numberOfLines={1}
+            >
+              {username}
+            </Text>
+          </View>
+        </View>
+        <View className="pt-2">
+          <FontAwesomeIcon icon={faEllipsisVertical} size={25} color="white" />
+        </View>
+      </View>
+      {play ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity
+          className="w-full h-60 rounded-xl mt-6 relative 
+        justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => {
+            setPlay(true);
+          }}
+        >
+          <Image
+            source={{ uri: thumbnail }}
+            className="w-full h-full rounded-xl mt-3 "
+            resizeMode="cover"
+          />
+          <FontAwesomeIcon icon={faCirclePlay} size={80} style={styles.icon} />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+export default VideoCard;
+
+const styles = StyleSheet.create({
+  icon: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -40 }, { translateY: -40 }],
+    color: "#CDCDE0",
+  },
+});
