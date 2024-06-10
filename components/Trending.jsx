2911736e33b4748dd06974309ac3e099593e9551
Suhasini Padmanathan <suhasini_95@hotmail.com>
2024-06-09 22:37:45 -0400

    trending videos zoom in and out

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -13,13 +13,13 @@ const zoomIn = {
     scale: 0.9,
   },
   1: {
-    scale: 1,
+    scale: 1.1,
   },
 };
 
 const zoomOut = {
   0: {
-    scale: 1,
+    scale: 1.1,
   },
   1: {
     scale: 0.9,
@@ -56,7 +56,14 @@ const TrendingItem = ({ activeItem, item }) => {
 };
 
 const Trending = ({ posts }) => {
-  const [activeItem, setActiveItem] = useState(posts[0]);
+  const [activeItem, setActiveItem] = useState(posts[1]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
+
   return (
     <FlatList
       data={posts}
@@ -64,6 +71,11 @@ const Trending = ({ posts }) => {
       renderItem={({ item }) => (
         <TrendingItem activeItem={activeItem} item={item} />
       )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      contentOffset={{ x: 170 }}
       horizontal
     />
   );
