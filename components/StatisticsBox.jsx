6f0cf317d52ce4aeedffc48c6ecf914ebe2a406c
Suhasini Padmanathan <suhasini_95@hotmail.com>
2024-06-26 11:17:00 -0400

    chart aggregation for week and month logic is up. chart data not fetching accurately. need to refactor code for StatisticsBox

--- components/StatisticsBox.jsx
+++ components/StatisticsBox.jsx
@@ -16,22 +16,6 @@ const StatisticsBox = () => {
     isLoading,
     refetch,
   } = useAppwrite(() => getUserReceipts(user.$id, chartTimeFrame));
-  /* 
-  const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-    datasets: [
-      {
-        data: [20, 45, 28, 80, 99, 43],
-        color: (opacity = 1) => `rgba(13, 166, 194, ${opacity})`, // optional
-        strokeWidth: 3, // optional
-      },
-    ],
-  };
-    let duration = numDays * 24 * 60 * 60 * 1000; //duration in milliseconds
-    let startDate = Date.now(); //current time in milliseconds
-    let endDate = new Date(startDate - duration);
-
-*/
 
   const numDaysInMonth = (year, month) => new Date(year, month, 0).getDate();
 
@@ -66,28 +50,84 @@ const StatisticsBox = () => {
   };
 
   const loadChartData = async () => {
-    await refetch();
+    // console.log("timeframe: ", chartTimeFrame);
 
     // generate labels for given time frame
-    let data = [];
+    let data = {};
+    let totalsInOrder = [];
     let labels = generateLabels();
+
     if (!isLoading && receipts) {
-      let reversedReceipts = receipts.reverse();
-      reversedReceipts.forEach((item) => {
-        let date = new Date(item.purchaseDate);
-        if (chartTimeFrame == "Week") {
-          //aggregate total by day and push to data
-        } else if (chartTimeFrame === "Month") {
-          //aggregate total by week and push to data
-        } else if (chartTimeFrame === "Year") {
-          //aggrefate total by month and push to data
+      let currDate = new Date();
+
+      if (chartTimeFrame == "Week") {
+        //initialise all the week's date totals to zero. curr date is always the latest data
+        for (let i = 0; i < 7; i++) {
+          data[i] = 0;
         }
-      });
-      setChartData({ labels: labels, data: data });
+        //for each receipt, add it's total to the corresponding data key value pair
+        receipts.forEach((item) => {
+          let itemDate = new Date(item.purchaseDate);
+          data[itemDate.getDay()] += item.total;
+        });
+        for (let i = 0; i < 7; i++) {
+          // Calculate the day we want to get the total for
+          let day = (currDate.getDay() - i + 7) % 7;
+          totalsInOrder.push(data[day]);
+        }
+      } else if (chartTimeFrame === "Month") {
+        let weeklyTotals = [0, 0, 0, 0];
+
+        // Iterate through receipts and add their totals to the corresponding week
+        receipts.forEach((item) => {
+          let itemDate = new Date(item.purchaseDate);
+          let daysAgo = Math.floor(
+            (currDate - itemDate) / (1000 * 60 * 60 * 24)
+          );
+
+          if (daysAgo < 7) {
+            weeklyTotals[0] += item.total;
+          } else if (daysAgo < 14) {
+            weeklyTotals[1] += item.total;
+          } else if (daysAgo < 21) {
+            weeklyTotals[2] += item.total;
+          } else if (daysAgo < 28) {
+            weeklyTotals[3] += item.total;
+          }
+        });
+        // Add the weekly totals in reverse order to match the current day-first order
+        totalsInOrder = weeklyTotals;
+      }
+    } else if (chartTimeFrame === "Year") {
+      //aggregate total by month and push to data
+      let monthlyTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+      // // Iterate through receipts and add their totals to the corresponding week
+      // receipts.forEach((item) => {
+      //   let itemDate = new Date(item.purchaseDate);
+      //   let daysAgo = Math.floor(
+      //     (currDate - itemDate) / (1000 * 60 * 60 * 24)
+      //   );
+
+      //   if (daysAgo < 7) {
+      //     weeklyTotals[0] += item.total;
+      //   } else if (daysAgo < 14) {
+      //     weeklyTotals[1] += item.total;
+      //   } else if (daysAgo < 21) {
+      //     weeklyTotals[2] += item.total;
+      //   } else if (daysAgo < 28) {
+      //     weeklyTotals[3] += item.total;
+      //   }
+      // });
+      // Add the weekly totals in reverse order to match the current day-first order
+      totalsInOrder = monthlyTotals;
     }
+    // console.log("totals in order", totalsInOrder);
+    setChartData({ labels: labels, data: totalsInOrder });
   };
 
   useEffect(() => {
+    refetch();
     loadChartData();
     // console.log("chart data is: ", chartData);
   }, [chartTimeFrame]);
